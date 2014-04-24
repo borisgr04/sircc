feae1604192682ce94b2feec6ec236fb90799642
borisgr04 <borisgr04@gmail.com>
2014-04-24 00:02:35 +0000

    Se Implementa la consulta del Sirecci

---
+++  sircc --username borisgr04@gmail.com/Informes4/Sireci/RptSireci.aspx.vb
@@ -0,0 +1,14 @@
+﻿
+Partial Class Reportes_Sireci_RptSireci
+    Inherits System.Web.UI.Page
+
+
+    Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+        'Dim r As New RptSireci()
+
+        'GridView1.DataSource = r.GetInfSireci("01/01/2012", "01/12/2012")
+
+        'GridView1.DataBind()
+        'Response.Write(r.Msg)
+    End Sub
+End Class
