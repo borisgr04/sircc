2254701acb7cd79d2038b1d9309d680a7bc684af
anyamiyeth@byasystems.com.co <anyamiyeth@byasystems.com.co@86cf5d1c-d26a-6560-d677-2563740c9adb>
2015-05-30 19:12:33 +0000

    Se agrega consulta de Terceros por Consorcios.

---
+++ trunk/ sircc --username borisgr04@gmail.com/Consultas/ConConsorcios/ConConsorcios.aspx.vb
@@ -0,0 +1,20 @@
+﻿
+Partial Class Consultas_ConConsorcios_ConConsorcios
+    Inherits PaginaComun
+
+    Protected Sub BtnConsultar_Click(sender As Object, e As EventArgs) Handles BtnConsultar.Click
+        Dim f As String = txtFiltro.Text
+        Dim obj As New CtrConsultasConsorcios
+
+
+        grdConsorcios.DataSource = obj.GetConsorcios(f)
+        grdConsorcios.DataBind()
+
+
+    End Sub
+
+    Protected Sub grdConsorcios_SelectedIndexChanged(sender As Object, e As EventArgs) Handles grdConsorcios.SelectedIndexChanged
+        'Response.Redirect("Consultas/Contratos/Contratos.aspx?numero=" + grdConsorcios.SelectedValue)
+        Redireccionar_Pagina("/Consultas/Contratos/Contratos.aspx?numero=" + grdConsorcios.SelectedValue)
+    End Sub
+End Class
